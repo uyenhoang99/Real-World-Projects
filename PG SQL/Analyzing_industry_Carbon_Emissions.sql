0f4a1ee536a89312cf563ccb70ec0dc54ed4a258
uyenhoang99 <uhoang67@gmail.com>
2025-12-29 22:13:30 -0800

    update file

--- PG SQL/Analyzing_industry_Carbon_Emissions.sql
+++ PG SQL/Analyzing_industry_Carbon_Emissions.sql
@@ -1,3 +1,9 @@
+/*Using the product_emissions table, find the number of unique companies and their total carbon footprint PCF for each industry group, 
+filtering for the most recent year in the database. The query should return three columns: industry_group, num_companies, 
+and total_industry_footprint, with the last column being rounded to one decimal place. 
+The results should be sorted by total_industry_footprint from highest to lowest values.
+*/
+
 --query the product_emissions table
 SELECT * FROM public.product_emissions
 LIMIT 5;
